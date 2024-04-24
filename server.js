e61a9eb30c9809b51240682f5f63d3604101b927
Manan Doshi <manan.doshi@colorado.edu>
2024-04-24 01:01:01 -0600

    Resolving branch

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-// Data fetching Server
+// Data fetching Server -- Heroku integration
 
 app.get('/hello', (req, res) => {
   res.status(200).send("Hello World!")
